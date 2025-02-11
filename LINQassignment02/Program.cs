6ce6f2959d98fa6afc31652011d7767a901c5373
ayman <aymanehab2002@gmail.com>
2025-02-12 01:58:58 +0200

    LINQ - Element Operators Q01

--- LINQassignment02/Program.cs
+++ LINQassignment02/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace LINQassignment02
+﻿using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using static LINQassignment02.ListGenerator;
+
+namespace LINQassignment02
 {
     internal class Program
     {
         static void Main()
         {
-           
+            #region LINQ - Element Operators
+            #region Q01
+            var result = ProductsList.Where(p=>p.UnitsInStock==0).First();
+            Console.WriteLine(result);
+
+            #endregion
+
+            #endregion
         }
     }
 }
