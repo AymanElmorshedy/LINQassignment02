a1f1ddd4236479b656df0dbe4d60d76470a79868
ayman <aymanehab2002@gmail.com>
2025-02-12 02:10:22 +0200

    LINQ - Aggregate Operators Q02

--- LINQassignment02/Program.cs
+++ LINQassignment02/Program.cs
@@ -32,10 +32,48 @@ namespace LINQassignment02
             #region Q01
 
 
-            int[] Numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-            var result = Numbers.Count(n=>n%2==1);
-            Console.WriteLine(result);
+            //int[] Numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var result = Numbers.Count(n=>n%2==1);
+            //Console.WriteLine(result);
 
+            #endregion
+            #region Q02
+            var Result = CustomersList.Select(c => new { customerId = c.CustomerID, customerName = c.CustomerName, OrderCount = c.Orders.Count() });
+            foreach (var item in Result)
+                Console.WriteLine(item);
+            #endregion
+
+            #region Q03
+            //var result = ProductsList.GroupBy(p=>p.Category).Select(p=> new {categoryName=p.Key,productCount=p.Count()});
+            //foreach(var item in result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region Q04
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var resuly= Arr.Count();
+            //Console.WriteLine(resuly);
+            #endregion
+            //string[] dictionaryWords = File.ReadAllLines("dictionary_english.txt");
+            #region Q05
+
+            //var result = dictionaryWords.Count();
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Q06
+            //var result = dictionaryWords.Min(x => x.Length);
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Q07
+            //var result = dictionaryWords.Max(x => x.Length);
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Q08
+            //var result = dictionaryWords.Average(d=>d.Length);
+            //Console.WriteLine(result);
             #endregion
             #endregion
         }
